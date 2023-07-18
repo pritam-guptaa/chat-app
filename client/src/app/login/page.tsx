483c6199c6723233a1daa7e363a14b285cc5d757
pritam-guptaa <uniquepritam29@gmail.com>
2023-07-18 23:36:18 +0530

    updated login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,15 +1,23 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import Button from '@/components/ui/Button'
 
-interface pageProps {
+interface pageProps {    
   
 }
 
-const page: FC<pageProps> = ({}) => {
-  return <>
-    <div className='flex min-h-full items-center justify-center'>
+const Page: FC<pageProps> = ({}) => {
+    const [isLoading, setIsloading] = useState<boolean>()
 
+  return <>
+    <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='w-full flex flex-col items-center max-w-md space-y-8 '>
+            logo
+            <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
+        </div>
+
+        <Button isLoading={isLoading}></Button>
     </div>
   </>
 }
 
-export default page
+export default Page
