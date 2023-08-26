d8be51381c913e02518ab370681d4c2b092b94cb
pritam-guptaa <uniquepritam29@gmail.com>
2023-08-26 20:12:43 +0530

    added illustration in login page

--- client/src/components/MobileChatLayout.tsx
+++ client/src/components/MobileChatLayout.tsx
@@ -99,7 +99,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                       <div className="px-4 sm:px-6">
                         <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                          Dashboard
+                          Chat App
                         </Dialog.Title>
                       </div>
                       <div className="relative mt-6 flex-1 px-4 sm:px-6">
